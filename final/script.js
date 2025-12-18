393866051cfb7106085ae49437e7572890ad708c
zhouc658 <zhouc658@newschool.edu>
2025-12-17 23:59:55 -0500

    final

--- final/script.js
+++ final/script.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navItems = document.querySelectorAll('.nav-item');
+    const navItems = document.querySelectorAll('.nav');
     const sections = document.querySelectorAll('.section');
   
     navItems.forEach(item => {
       item.addEventListener('click', () => {
   
         // 1️⃣ Update nav buttons
-        navItems.forEach(nav => nav.classList.remove('active'));
-        item.classList.add('active');
+        navItems.forEach(nav => nav.classList.remove('on'));
+        item.classList.add('on');
   
         // 2️⃣ Update content sections
-        sections.forEach(section => section.classList.remove('active'));
+        sections.forEach(section => section.classList.remove('on'));
         const targetSection = Array.from(sections)
           .find(section => section.dataset.target === item.dataset.target);
-        targetSection.classList.add('active');
+        targetSection.classList.add('on');
   
       });
     });
